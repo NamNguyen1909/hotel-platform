20b61d6f53789b315d0166cbe9e2d80a94d71751
NamNguyen1909 <namnguyen19092004@gmail.com>
2025-07-08 23:32:42 +0700

    Update Header.jsx

--- hotelplatformweb-vite/src/components/Header.jsx
+++ hotelplatformweb-vite/src/components/Header.jsx
@@ -59,7 +59,7 @@ const menuItemsByRole = {
   ],
   owner: [
     { text: 'Dashboard', icon: <Dashboard />, path: '/dashboard' },
-    { text: 'Quản lý phòng', icon: <Hotel />, path: '/rooms' },
+    { text: 'Quản lý phòng', icon: <Hotel />, path: '/rooms-management' },
     { text: 'Quản lý khách hàng', icon: <People />, path: '/customers' },
     { text: 'Đặt phòng', icon: <Book />, path: '/bookings' },
     { text: 'Thanh toán', icon: <Payment />, path: '/payments' },
