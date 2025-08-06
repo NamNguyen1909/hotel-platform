858a6256ce82391a2c8488283d0fbe45383b6ebf
Nguyễn Phong Phú <npphus@gmail.com>
2025-08-06 14:04:38 +0700

    Update settings.py

--- hotelplatformapi/hotelplatformapi/settings.py
+++ hotelplatformapi/hotelplatformapi/settings.py
@@ -31,7 +31,6 @@ DEBUG = True
 ALLOWED_HOSTS = []
 
 # Application definition
-
 INSTALLED_APPS = [
     'django.contrib.admin',
     'django.contrib.auth',
@@ -57,7 +56,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
-    'oauth2_provider.middleware.OAuth2TokenMiddleware',  # Đặt sau cùng
+    'oauth2_provider.middleware.OAuth2TokenMiddleware',  # OAuth2 middleware
 ]
 
 ROOT_URLCONF = 'hotelplatformapi.urls'
@@ -81,8 +80,12 @@ WSGI_APPLICATION = 'hotelplatformapi.wsgi.application'
 
 # Database
 # https://docs.djangoproject.com/en/5.2/ref/settings/#databases
-
 DATABASES = {
+    # 'default': dj_database_url.config(
+    #     default=os.getenv('DATABASE_URL'),
+    #     conn_max_age=600,
+    #     ssl_require=True
+    # )
     'default': {
         'ENGINE': 'django.db.backends.mysql',
         'NAME': 'hoteldb',
@@ -94,7 +97,6 @@ DATABASES = {
 
 # Password validation
 # https://docs.djangoproject.com/en/5.2/ref/settings/#auth-password-validators
-
 AUTH_PASSWORD_VALIDATORS = [
     {
         'NAME': 'django.contrib.auth.password_validation.UserAttributeSimilarityValidator',
@@ -112,23 +114,17 @@ AUTH_PASSWORD_VALIDATORS = [
 
 # Internationalization
 # https://docs.djangoproject.com/en/5.2/topics/i18n/
-
 LANGUAGE_CODE = 'en-us'
-
 TIME_ZONE = 'UTC'
-
 USE_I18N = True
-
 USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.2/howto/static-files/
-
 STATIC_URL = 'static/'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
-
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 AUTH_USER_MODEL = 'hotelplatform.User'
@@ -141,7 +137,6 @@ cloudinary.config(
     secure=True
 )
 DEFAULT_FILE_STORAGE = 'cloudinary_storage.storage.MediaCloudinaryStorage'
-
 STATICFILES_STORAGE = 'cloudinary_storage.storage.StaticCloudinaryStorage'
 
 # OAuth2 Settings (sử dụng token bình thường)
@@ -154,6 +149,7 @@ OAUTH2_PROVIDER = {
     'REFRESH_TOKEN_EXPIRE_SECONDS': 3600 * 24 * 7,  # 7 days
     'AUTHORIZATION_CODE_EXPIRE_SECONDS': 600,  # 10 minutes
     'ROTATE_REFRESH_TOKEN': True,
+    # Thêm dòng này để hỗ trợ password grant type
     'APPLICATION_MODEL': 'oauth2_provider.Application',
     'GRANT_TYPES': {
         'authorization-code': 'oauth2_provider.grant_types.AuthorizationCodeGrantType',
@@ -198,9 +194,9 @@ REST_FRAMEWORK = {
         'rest_framework.parsers.FormParser',
     ],
     'DEFAULT_AUTHENTICATION_CLASSES': [
-        'rest_framework.authentication.SessionAuthentication',
-        'rest_framework.authentication.TokenAuthentication',
-        'rest_framework_simplejwt.authentication.JWTAuthentication',
+        'rest_framework_simplejwt.authentication.JWTAuthentication',  # JWT
+        'oauth2_provider.contrib.rest_framework.OAuth2Authentication',  # OAuth2
+        'rest_framework.authentication.SessionAuthentication',  # Session
     ],
     'DEFAULT_PERMISSION_CLASSES': [
         'rest_framework.permissions.AllowAny',  # Mặc định cho phép tất cả
@@ -212,14 +208,29 @@ REST_FRAMEWORK = {
 }
 
 CORS_ALLOW_ALL_ORIGINS = True  # Cho phép tất cả các nguồn gốc
-
 CORS_ALLOWED_ORIGINS = [
     'http://localhost:3000',  # React dev server
     'http://localhost:5173',  # Vite dev server
 ]
 
 CLIENT_ID = 'kycIFibcgbEopjJ9esSVPU5PTECw6z4jAqMZ5j9w'
-CLIENT_SECRECT = 'TI8kzKgylZvAmqmSoi0SpgYt4z0pBS3SzNEEEPey0tVpYXRBQJgfrsYQzakk433ONKTc8WF9q3FnZR0XtDI1aOkj5bsIJcL9hZvpHaxJH9vXOUklrGXaiPivlJPOYuN4'
+CLIENT_SECRET = 'TI8kzKgylZvAmqmSoi0SpgYt4z0pBS3SzNEEEPey0tVpYXRBQJgfrsYQzakk433ONKTc8WF9q3FnZR0XtDI1aOkj5bsIJcL9hZvpHaxJH9vXOUklrGXaiPivlJPOYuN4'
+
+# Cho Mobile App / Direct API: Sử dụng Simple JWT
+# POST /api/auth/token/
+# {
+#     "username": "user@example.com",
+#     "password": "password"
+# }
+
+# Cho Third-party Applications: Sử dụng OAuth2 flow
+# POST /o/token/
+# {
+#     "grant_type": "authorization_code",
+#     "code": "authorization_code",
+#     "client_id": "your_client_id",
+#     "client_secret": "your_client_secret"
+# }
 
 LOGGING = {
     'version': 1,
@@ -231,12 +242,12 @@ LOGGING = {
     },
     'root': {
         'handlers': ['console'],
-        'level': 'INFO',
+        'level': 'INFO',  # Đảm bảo log INFO trở lên được in ra
     },
     'loggers': {
         'hotelplatform': {
             'handlers': ['console'],
-            'level': 'INFO',
+            'level': 'INFO',  # Có thể đổi thành 'DEBUG' nếu muốn chi tiết hơn
             'propagate': False,
         },
         'django': {
